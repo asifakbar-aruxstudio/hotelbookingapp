dcf4f644d063d0ba5f989bd314a31f476904bc9b
asif ali <786asifakbar53@gmail.com>
2026-09-03 01:28:24 +0500

    add Navbar component and remove placeholder text from App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
 
   return (
     <>
-  <h1 class="text-3xl font-bold underline text-center mt-10 ">
-    Hello world!
-  </h1>
+     <Navbar />
     
     </>
   )
