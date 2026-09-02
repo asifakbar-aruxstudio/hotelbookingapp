ceda44b27d8158ef082bf2f6fcdda8cd80dc9f23
asif ali <786asifakbar53@gmail.com>
2026-09-03 01:38:53 +0500

    conditionally render Navbar based on owner path

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar'
 import './App.css'
+import { useLocation } from 'react-router-dom'
 
 function App() {
-
+const isOwnerPath = useLocation().pathname.includes('/owner');
   return (
     <>
-     <Navbar />
+     {!isOwnerPath && <Navbar />}
     
     </>
   )
