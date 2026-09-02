d545250697fab9b0360f9cf5867530eeb00aaa90
asif ali <786asifakbar53@gmail.com>
2026-09-02 16:26:31 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  <h1 class="text-3xl font-bold underline text-center mt-10 ">
+    Hello world!
+  </h1>
+    
+    </>
+  )
+}
+
+export default App
